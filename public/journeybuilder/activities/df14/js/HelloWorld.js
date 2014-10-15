a3696dd215d799af48cbf4052fd74ac5e51b7b7c
Jake Miller <jake.miller@in6516.et.local>
2014-10-15 00:37:43 -0400

    fix postmonger issue

--- public/journeybuilder/activities/df14/js/HelloWorld.js
+++ public/journeybuilder/activities/df14/js/HelloWorld.js
@@ -64,13 +64,7 @@ define([
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
 
-        // Disable the next button if a value isn't selected
-        $('#select1').change(function() {
-            var message = getMessage();
-            connection.trigger('updateButton', { button: 'next', enabled: Boolean(message) });
-
-            $('#message').html(message);
-        });
+        connection.trigger('updateButton', { button: 'next', enabled: false });
     }
 
     function gotoStep(step) {
